425571d618b6fe468c2d6b9709fcc739173a024e
Anatolii Pozniak <a13101990a@gmail.com>
2023-04-03 12:18:32 +0300

    Add staticfiles, styles css. Add pagination and sidebar template

--- do_to_do/settings.py
+++ do_to_do/settings.py
@@ -69,6 +69,8 @@ TEMPLATES = [
     },
 ]
 
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
 WSGI_APPLICATION = "do_to_do.wsgi.application"
 
 
@@ -118,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
